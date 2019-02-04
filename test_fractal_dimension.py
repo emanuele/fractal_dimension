ee8e42326c0a2a6822fcf3d3d6b42005b0612539
Emanuele Olivetti <emanuele.olivetti@gmail.com>
2019-02-04 12:21:57 +0100

    added box_size in example on 2D image

--- test_fractal_dimension.py
+++ test_fractal_dimension.py
@@ -11,13 +11,18 @@ if __name__ == '__main__':
     filename = 'Sierpinski_triangle.png'
     image = io.imread(filename)
     image = rgb2grey(image)
+    print("Image: %s" % (image.shape,))
 
     n_steps = 100
-    box_side_max = None  # np.min(image.shape) / 3.0
-    fractal_dimension, box_side, counts = compute_fractal_dimension(image, n_steps=n_steps, box_side_max=box_side_max)
+    box_size_max = None  # np.min(image.shape) / 3.0
+    box_size = np.arange(1, 100)
+    fractal_dimension, box_size, counts = compute_fractal_dimension(image,
+                                                                    n_steps=n_steps,
+                                                                    box_size_max=box_size_max,
+                                                                    box_size=box_size)
     print("Fractal dimension = %s" % fractal_dimension)
     if 'ierpinski' in filename:
         print("Expected fractal dimension = %s" % (np.log(3) / np.log(2)))
     
     plt.ion()
-    plot_fractal_dimension(box_side, counts)
+    plot_fractal_dimension(box_size, counts)
